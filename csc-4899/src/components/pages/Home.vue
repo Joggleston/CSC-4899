b25cd36b05e3ec032e9222cd8885f6be07c9567e
Peter Green <57639888+DookJob@users.noreply.github.com>
2022-09-20 14:57:41 -0400

    Basic Homepage
    Made login.bue SPA and the Home.vue
    
    Additionally readded main.js to allow project building for any finalized type release

--- csc-4899/src/components/pages/Home.vue
+++ csc-4899/src/components/pages/Home.vue
@@ -1,11 +1,17 @@
 <template>
+    <!-- basic html -->
     <div>
-
+        <h1>Unknown Canvas Site</h1>
     </div>
+    <Login/> <!-- login single page app -->
 </template>
 
 <script>
+import Login from '@/components/pages/Login.vue'
+
 export default {
-    
+    components: {
+        Login
+    }
 }
 </script>

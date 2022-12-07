87d665e6dd454383d58ef4fb3eecfa13d087c668
PatMarino <57639980+Pm01967@users.noreply.github.com>
2022-12-07 08:51:47 -0800

    Put username on the profile page

--- csc-4899/src/components/pages/Profile.vue
+++ csc-4899/src/components/pages/Profile.vue
@@ -1,8 +1,38 @@
+<script setup>
+        import { collection, query, getDocs, where } from "firebase/firestore";
+        import { db } from "/src/main.js";
+        import { getAuth } from "firebase/auth";
+
+        //grabs current loggedin users id
+        const auth = getAuth();
+        var user = auth.currentUser;
+
+        //error cause we try testing for uid before page finished loading
+        //sort database based on user id posts
+        // const q = query(collection(db, "Posts"), where("UUID", "==", user.uid));
+        // const Docs = await getDocs(q); 
+
+        // const postArray = [];
+        // Docs.forEach((doc) => {
+        //     postArray.push(doc.data());
+        // });
+        // console.log(Docs)
+        const username = [];
+        const usernameQ = await getDocs(query(collection(db,"Users"), where("UUID","==",user.uid)));
+        usernameQ.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+            username.push(doc.data())
+        });
+
+
+</script>
 <template>
     
     <body>
-
-        <h2>Profile</h2>
+        <div v-for="name in username">
+            <p class ="name">{{ name.Username }}</p>
+        </div>
+        
 
         <div v-for="post in postArray">
 
@@ -22,25 +52,16 @@
     </body>
 </template>
 
-<script setup>
-        import { collection, query, getDocs, where } from "firebase/firestore";
-        import { db } from "/src/main.js";
-        import { getAuth } from "firebase/auth";
+<style>
+.name {
+    color:white;
+    font-weight:200;
+    text-align: center;
+    padding: 0px;
+    background-color: #6667AB;
+    font-size:200%;
+    float:center;
+}
 
-        //grabs current loggedin users id
-        const auth = getAuth();
-        var user = auth.currentUser;
+</style>
 
-        //error cause we try testing for uid before page finished loading
-        //sort database based on user id posts
-        const q = query(collection(db, "Posts"), where("UUID", "==", user.uid));
-        const Docs = await getDocs(q); 
-
-        const postArray = [];
-        Docs.forEach((doc) => {
-            postArray.push(doc.data());
-            console.log(postArray)
-        });
-
-
-</script>
